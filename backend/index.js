914d9e5cc5d7b9a38c4c2516d81bd3bc4453010a
carl dungon <shiraishikamiko@gmail.com>
2025-03-30 16:37:53 +0800

    fixed2

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,11 @@ const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: [
+        "http://localhost:5173",
+        "https://studdy-buddy-maiin-production.up.railway.app",
+        "https://studdy-buddy-maiin.onrender.com"
+    ],
     credentials: true,
 }));
 
