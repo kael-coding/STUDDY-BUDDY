40024e03b62df27c23e557e7e298e54d826c03dd
carl dungon <shiraishikamiko@gmail.com>
2025-03-30 16:28:19 +0800

    fixer

--- frontend/src/store/taskStore.js
+++ frontend/src/store/taskStore.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import { create } from "zustand";
 
-const API_URL = import.meta.env.MODE === "development" ? "http://localhost:5000//api/schedule" : /api/schedule
-
+const API_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api/schedule" : /api/schedule
+axios.defaults.withCredentials = true;
 export const useTaskStore = create((set, get) => ({
     tasks: [],
     isLoading: false,
