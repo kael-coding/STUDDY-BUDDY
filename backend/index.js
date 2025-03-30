1951a1bbfa0531a13307901fc78e1fbde7f8ca3f
carl dungon <shiraishikamiko@gmail.com>
2025-03-30 14:18:23 +0800

    test

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,10 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.route.js';
-import sched_taskRoutes from './routes/Sched_task.route.js';
+import sched_taskRoutes from './routes/sched_task.route.js';
 import cors from 'cors';
-import schedule from './cron/scheduler.js';
-
+// Removed unused import
+import path from 'path';
 
 import { connectDB } from './db/connectDB.js';
 
@@ -13,6 +13,8 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true,
@@ -24,6 +26,15 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes)
 app.use("/api/schedule", sched_taskRoutes)
 
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, '/frontend/dist')))
+
+    app.get("*", (_, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    });
+
+}
 
 app.listen(PORT, () => {
     connectDB()
