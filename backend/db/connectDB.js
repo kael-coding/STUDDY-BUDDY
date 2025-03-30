e4550976f1b68491938ab878a26fd5fdd88ab7c4
carl dungon <shiraishikamiko@gmail.com>
2025-03-30 14:34:53 +0800

    config

--- backend/db/connectDB.js
+++ backend/db/connectDB.js
@@ -1,11 +1,15 @@
+import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+
+dotenv.config();
 
 export const connectDB = async () => {
     try {
+        console.log('Mongo URI:', process.env.MONGO_URI);
         const conn = await mongoose.connect(process.env.MONGO_URI);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
-        process.exit(1); //process code 1 code  means with failure, 0 means success
+        process.exit(1);
     }
 };
