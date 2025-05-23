28227d5f2d0444d2b6d56cb5237223430374c194
carl dungon <shiraishikamiko@gmail.com>
2025-05-23 23:10:57 +0800

    SOME TYPO FIXED

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -1,6 +1,6 @@
 // src/pages/IntroPage.jsx
 import { Link } from "react-router-dom";
-import Header from "../components/Header/header.jsx";
+import Header from "../components/header/header.jsx";
 import { useAuthStore } from "../store/authStore";
 
 const IntroPage = () => {
